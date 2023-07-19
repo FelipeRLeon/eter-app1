191b5eceffa2d8b644342aba691f5f8eee4bff5e
FelipeRLeon <feliiperestrepoleon@gmail.com>
2023-07-19 18:19:33 -0500

    step, setstep

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import Grower from "./components/Grower"
-import GrowType from "./components/GrowType"
-import PlantStrain from "./components/PlantStrain"
-import Nutrients from "./components/Nutrients"
-import Buds from "./components/Buds"
+import Grower from "./components/Grower";
+import GrowType from "./components/GrowType";
+import PlantStrain from "./components/PlantStrain";
+import Nutrients from "./components/Nutrients";
+import Buds from "./components/Buds";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState("nutrients")
 
   return (
     <div className='container'>
-      <Grower/>
-      <GrowType/>
-      <PlantStrain/>
-      <Nutrients/>
-      <Buds/>
+      {step === 'grower' && <Grower/>} 
+      {step === 'growtype' && <GrowType/>} 
+      {step === 'plantstrain' && <PlantStrain/>} 
+      {step === 'nutrients' && <Nutrients/>} 
+      {step === 'buds' && <Buds/>} 
     </div>    
   )
 }
---
+++ src/context/GrowerContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const GrowerContext = createContext();
+
+
+export default GrowerContext;
